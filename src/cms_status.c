70bebca7f37f99886e074fab2c4cab9a591f5e37
mengtechdigital <mengtechdigital@gmail.com>
2025-11-24 20:59:32 +0800

    chore: Moved cms_print_declaration to cms_status

--- src/cms_status.c
+++ src/cms_status.c
@@ -38,6 +38,26 @@ void cms_print_status(CMS_STATUS status)
 
 CMS_STATUS cms_print_declaration(void)
 {
-    /* TODO: Implement declaration printing */
-    return CMS_STATUS_NOT_IMPLEMENTED;
+    printf("Declaration\n");
+    printf("SIT's policy on copying does not allow the students to copy source code as well as assessment solutions\n");
+    printf("from another person AI or other places. Meanwhile, the students must also ensure that their work is\n");
+    printf("not accessible by others.\n\n");
+    printf("We hereby declare that:\n");
+    printf("- We fully understand and agree to the abovementioned plagiarism policy.\n");
+    printf("- We did not copy any code from others or from other places.\n");
+    printf("- We did not share our codes with others or upload to any other places for public access and will\n");
+    printf("not do that in the future.\n");
+    printf("- We agree that our project will receive Zero mark if there is any plagiarism detected.\n");
+    printf("- We agree that we will not disclose any information or material of the group project to others or\n");
+    printf("upload to any other places for public access.\n");
+    printf("- We agree that we did not copy any code directly from AI generated sources\n\n");
+    printf("Declared by: Team P11-6\n");
+    printf("Team members:\n");
+    printf("1. Cody Tan De Xuan (2503322)\n");
+    printf("2. Gerome Wong (2503678)\n");
+    printf("3. Gabriel Lee Hon Fei (2501901)\n");
+    printf("4. Natalie Peh (2501526)\n");
+    printf("5. Randy See Meng Teck (2500605)\n");
+    printf("Date: 23/11/2025\n\n");
+    return 1;
 }
